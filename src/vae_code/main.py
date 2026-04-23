569314015933a586fc66f1368618f1f1725ecb6e
Gavin Petruzzi <gape5988@colorado.edu>
2026-04-23 11:25:44 -0600

    Saving models, generating multiple ouputs, cleaning up code

--- src/vae_code/main.py
+++ src/vae_code/main.py
@@ -38,12 +38,12 @@ def get_desired_training_genre(config: VAEConfig) -> str:
         ]
 
         if genre in valid_genres:
-            print(f"Audio filepath set to: ./data/Data/genres_original/{genre}")
-            return f"./data/Data/genres_original/{genre}"
+            print(f"Audio filepath set to: {config.audio_dir}/{genre}")
+            return f"{config.audio_dir}/{genre}"
 
         print("Invalid genre. Please choose a valid genre.")
 
-def get_desired_training_epochs(config: VAEConfig) -> str:
+def get_desired_training_epochs() -> int:
      while True:
             try:
                 input_epochs = int(input("Enter number of epochs: "))
@@ -58,67 +58,203 @@ def get_desired_training_epochs(config: VAEConfig) -> str:
             except ValueError:
                 print("Please enter a valid integer.")
 
+def get_desired_songs_to_generate() -> int:
+    while True:
+        try:
+            num_songs_to_generate = int(input("Enter number of songs to generate: "))
+
+            if num_songs_to_generate <= 0:
+                print("Number of songs must be greater than 0.")
+                continue
+
+            return num_songs_to_generate
+
+        except ValueError:
+            print("Please enter a valid integer.")
+
+def save_models(config: VAEConfig, encoder: Encoder, decoder: Decoder) -> None:
+    while True:
+        save_model = input("Do you want to save the trained models? (y/n): ").strip().lower()
+
+        if save_model in ["y", "yes"]:
+            os.makedirs("saved_vae_models", exist_ok=True)
+
+            genre_name = os.path.basename(config.audio_dir)
+
+            encoder.save_weights(
+                f"saved_vae_models/{genre_name}_encoder.weights.h5"
+            )
+
+            decoder.save_weights(
+                f"saved_vae_models/{genre_name}_decoder.weights.h5"
+            )
+
+            print("Model weights saved successfully.")
+            break
+
+        elif save_model in ["n", "no"]:
+            print("Model weights were not saved.")
+            break
+
+        else:
+            print("Please enter y or n.")
+    
+def load_existing_models() -> tuple[str, str, str]:
+    valid_genres = [
+        "blues",
+        "classical",
+        "country",
+        "disco",
+        "hiphop",
+        "jazz",
+        "metal",
+        "pop",
+        "reggae",
+        "rock",
+    ]
+
+    while True:
+        genre = input(
+            "Which saved model would you like to load? "
+        ).strip().lower()
+
+        if genre not in valid_genres:
+            print("Invalid genre.")
+            continue
+
+        encoder_path = f"saved_vae_models/{genre}_encoder.weights.h5"
+        decoder_path = f"saved_vae_models/{genre}_decoder.weights.h5"
+
+        if os.path.exists(encoder_path) and os.path.exists(decoder_path):
+            print(f"Found saved {genre} model.")
+            return genre, encoder_path, decoder_path
+
+        print(f"No saved model found for {genre}.")
+
+
 def main():
     # Load the default config variables
     config = VAEConfig()
 
-    config.audio_dir = get_desired_training_genre(config)
-
-    config.epochs = get_desired_training_epochs(config)
-
     preprocessor = MelPreprocessor(config)
 
-    audio_files = find_audio_files(config.audio_dir)
-    print("Found", len(audio_files), "audio files from the path ", config.audio_dir)
-
-    X, skipped = build_dataset(
-        audio_files,
-        preprocessor,
+    dummy_sample = preprocessor.preprocess_file(
+        "./data/Data/genres_original/blues/blues.00000.wav"
     )
 
-    print("Usable files:", len(X))
-    print("Skipped files:", skipped)
+    h, w = dummy_sample.shape[0], dummy_sample.shape[1]
 
-    if len(X) == 0:
-        raise ValueError("No usable files found.")
+    while True:
+        model_choice = input(
+            "Do you want to (1) train a new model or (2) load an existing model? Enter 1 or 2: "
+        ).strip()
 
-    print("Input shape:", X.shape[1:])
+        if model_choice == "1":
+            train_new_model = True
+            break
 
-    h, w = X.shape[1], X.shape[2]
+        elif model_choice == "2":
+            train_new_model = False
+            break
 
-    dataset = (
-        tf.data.Dataset
-        .from_tensor_slices(X)
-        .shuffle(len(X))
-        .batch(config.batch_size)
-    )
+        else:
+            print("Please enter 1 or 2.")
 
-    encoder = Encoder(config.latent_dim)
-    decoder = Decoder(
-        config.latent_dim,
-        output_shape=(h, w),
-    )
+    if train_new_model:
+        config.audio_dir = get_desired_training_genre(config)
 
-    train_vae(
-        encoder,
-        decoder,
-        dataset,
-        config,
-    )
+        config.epochs = get_desired_training_epochs()
 
-    reconstruct_example(
-        encoder,
-        decoder,
-        X[0],
-        preprocessor,
-    )
+        # Rebuild preprocessor for config changes
+        preprocessor = MelPreprocessor(config)
 
-    generate_example(
-        decoder,
-        config,
-        preprocessor,
-    )
+        audio_files = find_audio_files(config.audio_dir)
+        print("Found", len(audio_files), "audio files from the path ", config.audio_dir)
 
+        X, skipped = build_dataset(
+            audio_files,
+            preprocessor,
+        )
+
+        print("Usable files:", len(X))
+        print("Skipped files:", skipped)
+
+        if len(X) == 0:
+            raise ValueError("No usable files found.")
+
+        print("Input shape:", X.shape[1:])
+
+        dataset = (
+            tf.data.Dataset
+            .from_tensor_slices(X)
+            .shuffle(len(X))
+            .batch(config.batch_size)
+        )
+
+        encoder = Encoder(config.latent_dim)
+        decoder = Decoder(
+            config.latent_dim,
+            output_shape=(h, w),
+        )
+
+        train_vae(
+            encoder,
+            decoder,
+            dataset,
+            config,
+        )
+
+    else:
+    # Load existing model 
+        genre, encoder_path, decoder_path = load_existing_models()
+        config.audio_dir = f"./data/Data/genres_original/{genre}"
+        preprocessor = MelPreprocessor(config)
+
+        encoder = Encoder(config.latent_dim)
+        decoder = Decoder(
+            config.latent_dim,
+            output_shape=(h, w)
+        )
+
+        dummy_x = tf.zeros((1, h, w, 1))
+        encoder(dummy_x)
+
+        dummy_z = tf.zeros((1, config.latent_dim))
+        decoder(dummy_z)
+
+        encoder.load_weights(encoder_path)
+        decoder.load_weights(decoder_path)
+
+        print("Model loaded successfully.")
+
+    
+    num_songs_to_generate = get_desired_songs_to_generate()
+
+    genre_name = os.path.basename(config.audio_dir)
+    output_dir = f"generated_songs/{genre_name}_songs"
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    for i in range(num_songs_to_generate):
+        generate_example(
+            decoder,
+            config,
+            preprocessor,
+            f"{output_dir}/generated_{genre_name}_{i + 1}.wav"
+        )
+
+    # reconstruct_example(
+    #     encoder,
+    #     decoder,
+    #     X[0],
+    #     preprocessor,
+    # )
+
+    # TODO: Pass generated songs into basic models to evaluate vae output accuracy
+
+    # Only ask to save model if it is not already saved
+    if train_new_model:
+        save_models(config, encoder, decoder)
 
 if __name__ == "__main__":
     main()
